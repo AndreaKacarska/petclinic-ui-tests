8cbaf88af1d8ab7b39b16c135f4e97e7f00c1ef1
rrr1113 <radicastojkoska1306@gmail.com>
2026-04-19 21:57:33 +0200

    Refactoring the code using sustainable practices

--- src/test/java/com/collaboration/petclinic/ui/pages/VisitsPage.java
+++ src/test/java/com/collaboration/petclinic/ui/pages/VisitsPage.java
@@ -1,118 +1,114 @@
 package com.collaboration.petclinic.ui.pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
 
 import java.time.Duration;
 
-/**
- * Page Object Model for Visits page
- * Encapsulates all locators and interactions related to visits management
- */
 public class VisitsPage {
-    private WebDriver driver;
-    private static final int TIMEOUT_SECONDS = 10;
+    private final WebDriver driver;
+    private final WebDriverWait wait;
 
-    private static final String ADD_VISIT_BUTTON_XPATH = "//dd[text()='%s']/following::button[text()='Add Visit']";
-    private static final String DATE_INPUT_XPATH = "//input[@ng-reflect-name='date']";
-    private static final String DESCRIPTION_INPUT_XPATH = "//input[@ng-reflect-name='description']";
-    private static final String ADD_VISIT_SUBMIT_BUTTON_XPATH = "//button[text()='Add Visit']";
-    private static final String UPDATE_VISIT_BUTTON_XPATH = "//button[text()='Update Visit']";
-    private static final String VISIT_DESCRIPTION_XPATH = "//td[contains(text(),'%s')]";
-    private static final String EDIT_VISIT_BUTTON_XPATH = "//td[contains(text(),'%s')]/following-sibling::td//button[text()='Edit Visit']";
-    private static final String DELETE_VISIT_BUTTON_XPATH = "//td[contains(text(),'%s')]/following-sibling::td//button[text()='Delete Visit']";
+    private static final int TIMEOUT_SECONDS = 10;
 
     public VisitsPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT_SECONDS));
     }
 
+    private static final String ADD_VISIT_BUTTON = "//dd[text()='%s']/following::button[text()='Add Visit']";
+    private static final By DATE_INPUT = By.xpath("//input[@ng-reflect-name='date']");
+    private static final By DESCRIPTION_INPUT = By.xpath("//input[@ng-reflect-name='description']");
+    private static final By ADD_VISIT_SUBMIT = By.xpath("//button[text()='Add Visit']");
+    private static final By UPDATE_VISIT_SUBMIT = By.xpath("//button[text()='Update Visit']");
+
+    private static final String VISIT_DESC = "//td[contains(text(),'%s')]";
+    private static final String EDIT_BTN = "//td[contains(text(),'%s')]/following-sibling::td//button[text()='Edit Visit']";
+    private static final String DELETE_BTN = "//td[contains(text(),'%s')]/following-sibling::td//button[text()='Delete Visit']";
+
+    private WebElement find(By locator) {
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    private void click(By locator) {
+        WebElement el = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(locator));
+            el.click();
+        } catch (ElementClickInterceptedException e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block:'center'});", el);
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", el);
+        }
+    }
+
+    private void type(By locator, String text) {
+        WebElement el = find(locator);
+        el.clear();
+        el.sendKeys(text);
+    }
+
+
     public void clickAddVisitButton(String petName) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT_SECONDS));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//dd[text()='" + petName + "']")));
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(String.format(ADD_VISIT_BUTTON_XPATH, petName))));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true);", button);
-        button.click();
+        click(By.xpath(String.format(ADD_VISIT_BUTTON, petName)));
     }
 
     public void enterDate(String date) {
-        WebElement dateInput = driver.findElement(By.xpath(DATE_INPUT_XPATH));
-        dateInput.clear();
-        dateInput.sendKeys(date);
+        type(DATE_INPUT, date);
     }
 
     public void enterDescription(String description) {
-        WebElement descInput = driver.findElement(By.xpath(DESCRIPTION_INPUT_XPATH));
-        descInput.clear();
-        descInput.sendKeys(description);
+        type(DESCRIPTION_INPUT, description);
     }
 
     public void submitAddVisit() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT_SECONDS));
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(ADD_VISIT_SUBMIT_BUTTON_XPATH)));
-        button.click();
+        click(ADD_VISIT_SUBMIT);
     }
 
     public void submitUpdateVisit() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT_SECONDS));
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(UPDATE_VISIT_BUTTON_XPATH)));
-        button.click();
+        click(UPDATE_VISIT_SUBMIT);
     }
 
     public boolean visitExists(String description) {
         try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT_SECONDS));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(String.format(VISIT_DESCRIPTION_XPATH, description))));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.xpath(String.format(VISIT_DESC, description))));
             return true;
-        } catch (Exception e) {
+        } catch (TimeoutException e) {
             return false;
         }
     }
 
-    public void clickEditVisitButton(String visitDescription) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT_SECONDS));
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(String.format(EDIT_VISIT_BUTTON_XPATH, visitDescription))));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true);", button);
-        button.click();
+    public void clickEditVisitButton(String description) {
+        click(By.xpath(String.format(EDIT_BTN, description)));
     }
 
-    public void clickDeleteVisitButton(String visitDescription) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT_SECONDS));
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(String.format(DELETE_VISIT_BUTTON_XPATH, visitDescription))));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true);", button);
-        button.click();
+    public void clickDeleteVisitButton(String description) {
+        click(By.xpath(String.format(DELETE_BTN, description)));
     }
 
-    public boolean waitForVisitDelete(String visitDescription) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT_SECONDS));
+    public boolean waitForVisitDelete(String description) {
         try {
-            return wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(String.format(VISIT_DESCRIPTION_XPATH, visitDescription))));
-        } catch (Exception e) {
+            return wait.until(ExpectedConditions.invisibilityOfElementLocated(
+                    By.xpath(String.format(VISIT_DESC, description))));
+        } catch (TimeoutException e) {
             return false;
         }
     }
 
     public boolean isAddVisitButtonEnabled() {
         try {
-            return driver.findElement(By.xpath(ADD_VISIT_SUBMIT_BUTTON_XPATH)).isEnabled();
+            return find(ADD_VISIT_SUBMIT).isEnabled();
         } catch (Exception e) {
             return false;
         }
     }
 
-    public void clearDateInput() {
-        WebElement dateInput = driver.findElement(By.xpath(DATE_INPUT_XPATH));
-        dateInput.clear();
-    }
-
     public void clearDescriptionInput() {
-        WebElement descInput = driver.findElement(By.xpath(DESCRIPTION_INPUT_XPATH));
-        descInput.clear();
+        find(DESCRIPTION_INPUT).clear();
     }
 }
+
+
+
+
