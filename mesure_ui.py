9325f1368519ed32db750b73ab4376a77dcd696c
Маринов Андреј <andrej.marinov@students.finki.ukim.mk>
2026-04-23 22:12:07 +0200

    Add emissions tracking for UI tests and update output files

--- mesure_ui.py
+++ mesure_ui.py
@@ -1,12 +1,19 @@
 from codecarbon import EmissionsTracker
 import subprocess
 
-tracker = EmissionsTracker(project_name="PetClinic-UI-Tests", output_file="ui_emissions.csv")
+tracker = EmissionsTracker(project_name="PetClinic-UI-Tests", output_file="ui_with_AI_emissions.csv")
 
 tracker.start()
 
+# Run the UI tests
+# subprocess.run(
+#     '"C:\\Program Files\\JetBrains\\IntelliJ IDEA 2025.3\\plugins\\maven\\lib\\maven3\\bin\\mvn" test -Dtest=AddOwnerTest',
+#     shell=True
+# )
+
+# Run the AI-assisted UI tests
 subprocess.run(
-    '"C:\\Program Files\\JetBrains\\IntelliJ IDEA 2025.3\\plugins\\maven\\lib\\maven3\\bin\\mvn" test -Dtest=AddOwnerTest',
+    '"C:\\Program Files\\JetBrains\\IntelliJ IDEA 2025.3\\plugins\\maven\\lib\\maven3\\bin\\mvn" test -Dtest=AddOwnerUITestsWithAI',
     shell=True
 )
 
